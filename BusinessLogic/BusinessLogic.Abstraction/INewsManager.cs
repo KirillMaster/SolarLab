b779111a7ea9d5bd8496655f8864d6460c0f4b9e
KirillMaster <suhorukih@mail.ru>
2016-12-19 15:20:24 +0300

    Добавил DAL и BLL скелет

---
+++ BusinessLogic/BusinessLogic.Abstraction/INewsManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLogic.Abstraction
+{
+
+    /// <summary>
+    /// Manager to work with news
+    /// </summary>
+    public interface INewsManager
+    {
+        /// <summary>
+        /// Add news
+        /// </summary>
+        void AddNews();
+    }
+
+}
