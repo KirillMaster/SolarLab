fb5ae1fa35f7d1da57e347bbb4598f58c5247187
KirillMaster <suhorukih@mail.ru>
2017-01-08 20:36:13 +0300

    Создание интерфейсов IRepository, IUnitOfWork и базового класса для репозиториев

---
+++ DataAccessLayer/DataAccessLayer.Repositories/DataAccessLayer.Repositories/Core/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Document> Documents { get; }
+        IRepository<File> Files { get; }
+        IRepository<Group> Groups { get; }
+        IRepository<Post> Posts { get; }
+        IRepository<AppTask> Tasks { get; }
+
+        IRepository<Employee> Employees { get; }
+        void Save();
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer.Repositories/DataAccessLayer.Repositories/Core/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using DataAccessLayer;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Repositories.Core
+{
+    public class RepositoryBase<T> where T :  class
+    {
+        protected ApplicationContext dbContext = new ApplicationContext();
+        protected DbSet<T> entityContext;
+
+        protected RepositoryBase()
+        {
+            entityContext = dbContext.Set<T>();
+        }
+        public IEnumerable<T> Get()
+        {
+            return entityContext.AsEnumerable();
+        }
+        public IEnumerable<T> Get(Expression<Func<T, bool>> expression)
+        {
+            return entityContext.Where(expression).AsEnumerable();
+        }
+        public T Get(int id)
+        {
+            return entityContext.Find(id);
+        }
+        public void Add(T entity)
+        {
+            entityContext.Add(entity);
+        }
+        public void Update(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+        }
+        public void Delete(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Deleted;
+        }
+
+ 
+
+    }
+}
